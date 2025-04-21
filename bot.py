f4448580189bdec865d16a702da26079e3ed33ae
BuildTools <unconfigured@null.spigotmc.org>
2025-04-21 16:09:51 +0200

    feat(command): add update and add command && autocomplete full OP

--- bot.py
+++ bot.py
@@ -39,7 +39,7 @@ async def on_message(message : discord.Message):
         await gen_channel.send("Hello there !")
 
 ##region Info command
-@bot.tree.command(name="info", description="Tester les embeds")
+@bot.tree.command(name="info", description="Get the information card of a character")
 async def info(interaction: discord.Interaction, name: str):
     embed = discord.Embed(
         title=name.capitalize(),
@@ -64,12 +64,53 @@ async def name_autocomplete(
 ) -> List[discord.app_commands.Choice[str]]:
     names = []
     for val in collection.find({'name': {'$ne': None}}):
-        names.append(val["name"].capitalize())
+        if current.lower() in val["name"].lower():
+            names.append(val["name"].capitalize())
     return [
         discord.app_commands.Choice(name=name, value=name)
         for name in names
     ]
+##endregion
 
+def exist(name: str) -> bool:
+    if collection.find_one({"name": name.lower()}) is None:
+        return False
+    return True
+
+##region Add character
+@bot.tree.command(name="add", description="Add new character to the database")
+async def add(interaction: discord.Interaction, name: str, card_url: str):
+    if not exist(name):
+        collection.insert_one({"name": name.lower(), "url": card_url})
+        await interaction.response.send_message("Personnage ajouter avec succès.")
+        return
+    await interaction.response.send_message("Ce personnage est déjà enregistré.")
+##endregion
+
+##region Update character
+@bot.tree.command(name="update", description="Update card url of character in the database")
+async def update(interaction: discord.Interaction, name: str, card_url: str):
+    if exist(name):
+        filter = {"name": name.lower()}
+        new_value = { "$set": { "url": card_url } }
+        collection.update_one(filter, new_value)
+        await interaction.response.send_message("Personnage modifié avec succès.")
+        return
+    await interaction.response.send_message("Ce personnage n'existe pas, créé le avec la commande /add.")
+
+@update.autocomplete('name')
+async def name_autocomplete(
+        interaction: discord.Interaction,
+        current: str
+) -> List[discord.app_commands.Choice[str]]:
+    names = []
+    for val in collection.find({'name': {'$ne': None}}):
+        if current.lower() in val["name"].lower():
+            names.append(val["name"].capitalize())
+    return [
+        discord.app_commands.Choice(name=name, value=name)
+        for name in names
+    ]
 ##endregion
 
 bot.run(os.getenv('DISCORD_TOKEN'))
